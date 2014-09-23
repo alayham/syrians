0ae525076b962745a03179ad21b8e6ba9541af11
Server <alayham@hotmail.com>
2014-09-24 00:50:17 +0300

    Testing a new sharing function

--- www/syrians.js
+++ www/syrians.js
@@ -1,3 +1,6 @@
+function syrians_share_app(){
+	navigator.share('أدعوك للاطلاع على دليل المغتربين السوريين https://play.google.com/store/apps/details?id=com.alayham.syrians');	
+}
 $(document).on('click', '[rel="external"],.linksource', function (e) {
     e.preventDefault();
     var targetURL = $(this).attr("href");
