647b3fb26f86aadef4734c50efa8053f61f4088d
Server <alayham@hotmail.com>
2014-09-26 01:17:59 +0300

    Implemented a theme system and added the first theme (shadi)

--- www/syrians.js
+++ www/syrians.js
@@ -1,9 +1,26 @@
 function syrians_share_app(){
 	navigator.share('أدعوك للاطلاع على دليل المغتربين السوريين https://play.google.com/store/apps/details?id=com.alayham.syrians');	
 }
+
+function updatetheme(){
+	  localStorage.setItem('apptheme',$( "input:radio[name=apptheme]:checked" ).val());
+	  location.reload();
+}
 $(document).on('click', '[rel="external"],.linksource', function (e) {
     e.preventDefault();
     var targetURL = $(this).attr("href");
 
     window.open(targetURL, "_system");
 });
+
+var themestylesheet=localStorage.getItem('apptheme');
+if (themestylesheet.length<3){
+  localStorage.setItem(apptheme,'syria');
+  themestylesheet=localStorage.getItem(apptheme);
+}
+$("<link/>", {
+     rel: "stylesheet",
+     type: "text/css",
+     href: "jquery-mobile/themes/" + localStorage.getItem('apptheme') + ".css"
+  }).appendTo("head");
+
