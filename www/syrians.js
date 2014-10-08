d35b5cf93293cd133c5f10d28de4ed80f1991101
Server <alayham@hotmail.com>
2014-10-08 22:06:31 +0300

    New structure for the home page, with optional sections and links

--- www/syrians.js
+++ www/syrians.js
@@ -21,6 +21,109 @@ function updatetheme(){
 	  location.reload();
 }
 
+function homesection(sectionid,sectiontitle){
+	this.id=sectionid;
+	this.title=sectiontitle;
+	this.varname='section' + this.id;
+	this.html="";
+	this.links = [];
+	
+	if(localStorage.getItem(this.varname) == 1){
+		this.selected = " checked";
+	}else{
+		if(localStorage.getItem(this.varname) == 0){
+			this.selected = "";
+		}else{ //undefinded, show by default;
+			this.selected = " checked";
+			localStorage.setItem(this.varname,'1');
+		}
+	}
+	this.addlink=function(linkurl,linktitle){
+		this.links[this.links.length] = [linkurl,linktitle,linkurl.replace(/[^a-z0-9]/gi,'')]
+	}
+	this.renderoption=function(){
+		var txt = '';
+		for(j=0; j<this.links.length;j++){
+			txt += $("<div/>").append(
+				$("<input/>",{
+				type: 'checkbox',
+				"data-mini": "true",
+				name: this.links[j][2],
+				id: this.links[j][2],
+				checked: (localStorage.getItem(this.links[j][2]) == 0 ? false : true),
+			}).addClass('linkvisibility')).append($('<label/>',{for: this.links[j][2]}).text(this.links[j][1])).html();
+			
+		}
+		var opt=$('<div/>').append(
+			$('<input/>',{
+				type: "checkbox",
+				name: this.varname,
+				id: this.varname,
+				checked: (localStorage.getItem(this.varname) == 0 ? false : true),
+				'data-mini' : "true"
+			}).addClass("sectionvisibility")
+			).append(
+			$('<label/>',{
+				for: this.varname,
+			}).text(this.title)
+			).append($('<div/>',{
+				id: this.varname + "sublinks",
+				"data-role": "controlgroup"
+			}).addClass("sublinks").css("display", localStorage.getItem(this.varname) == 0 ? "none" : "block").html(txt)
+			).html();
+		console.log(opt);
+		return(opt);
+	}	
+	this.rendersection=function(){
+		if(localStorage.getItem(this.varname) == 0){
+			return("");
+		}
+		var txt = "";
+		txt += '<div id="' + this.varname + '" class="homecontainer" data-role="collapsible" data-inset="false" data-mini="true" data-collapsed="false">' +
+	  	    	'<legend>' + this.title + '</legend>' +
+	  	    	'<div class="ui-grid-a ui-responsive collapsiblecontent">';
+		txt += this.html;
+		var linknum=0;
+		for(i=0; i<this.links.length; i++){
+			if(localStorage.getItem(this.links[i][2]) == 0){
+				
+			}else{
+				txt += '<a id="' + this.links[i][2]  +   '" class="ui-block-' + (linknum % 2 == 0 ? "a" : "b") + ' homebutton" data-mini="true" data-role="button" data-ajax="false" href="' + this.links[i][0] +  '">' + this.links[i][1] + '</a>'
+				linknum++;
+			}
+		}
+	  	txt += '</div></div>';
+	  	return(txt);
+	}
+}
+function updatesections(){
+    for (i = 0; i < sectionlist.length; i++) {
+    	localStorage.setItem(sectionlist[i].varname,($('#' + sectionlist[i].varname + '').is(':checked') ? 1 : 0));
+  	}      
+}
+var sectionlist = [];
+
+sectionlist[0]=	new homesection("tv", "التلفزيون السوري");
+sectionlist[1]=	new homesection("intheworld", "السوريون في العالم");
+sectionlist[2]=	new homesection("connectsyria", "التواصل مع سوريا");
+sectionlist[3]=	new homesection("culture", "تقافة وتعليم");
+sectionlist[4]=	new homesection("about", "عن التطبيق");
+
+sectionlist[0].html ='<iframe width="100%" height="250" src="http://www.youtube.com/embed/videoseries?list=PLdnH0tm73Ekm5yvRzmWNbj7e-1ffaAaML" frameborder="0" allowfullscreen></iframe>';
+sectionlist[1].addlink('syria_embassy.html','دليل السفارات السورية');
+sectionlist[1].addlink('counsular_services_outside.html','دليل الخدمات القنصلية');
+sectionlist[1].addlink('military_service.html','شؤون التجنيد');
+sectionlist[1].addlink('expat_associations.html','النوادي السورية في العالم');
+sectionlist[2].addlink('live_syrian_tv.html','التلفزيون والإذاعة');
+sectionlist[2].addlink('directory.html','دليل المواقع السورية');
+sectionlist[2].addlink('invest_in_syria.html','دليل الاستثمار والأعمال');
+sectionlist[2].addlink('donate_to_syria.html','إرسال التبرعات');
+sectionlist[3].addlink('about_syria.html','معلومات عن سوريا');
+sectionlist[3].addlink('schoolbag.html','الحقيبة الالكترونية للكتب المدرسية');
+sectionlist[3].addlink('colors.html','تعليم الألوان للأطفال');
+sectionlist[4].addlink('whatsnew.html','ما الجديد');
+sectionlist[4].addlink('about.html','عن التطبيق');
+
 $(document).on('click', '[rel="external"],.linksource', function (e) {
     e.preventDefault();
     var targetURL = $(this).attr("href");
@@ -31,11 +134,19 @@ $(document).on('click', '[rel="external"],.linksource', function (e) {
 
 $(document).ready(function(){
 	$('.pagefooter').load('includes/footer.html');
+	$('.linkvisibility').click(function(){
+		localStorage.setItem($(this).attr('id'),($(this).is(':checked') ? 1 : 0));
+	});
+	$('.sectionvisibility').change(function(){
+		localStorage.setItem($(this).attr('id'),($(this).is(':checked') ? 1 : 0));
+		$('#' + $(this).attr('id') + "sublinks").toggle();
+	})
 });
 
 if (!localStorage.getItem('apptheme')){
   localStorage.setItem('apptheme','syria');
 }
+
 $("<link/>", {
      rel: "stylesheet",
      type: "text/css",
